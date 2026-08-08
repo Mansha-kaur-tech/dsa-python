45e3042222a6d41a496fe9f2c541b3f9cda40636
Mansha Kaur <mansha-kaur-tech@gmail.com>
2026-08-08 20:03:39 +0530

    linear-search

---
+++ arrays/linear-search.py
@@ -0,0 +1,20 @@
+# ------------------------------
+# Linear Search
+# ------------------------------
+
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1    # if the target isn't found in there then it prints -1
+
+
+# ------------------------------
+# Examples
+# ------------------------------
+print(linear_search([1, 2, 3, 4, 5], 3))  # returns 2)
+print()
+print(linear_search([1, 2, 3, 4, 5], 6))  # returns -1
+print()
+print(linear_search(['a', 'b', 'c', 'd'], 'd'))  # returns 2
+print()
