b1b121461e6b78dc7fd265e88282b0051cc72fc8
Mansha Kaur <mansha-kaur-tech@gmail.com>
2026-08-10 12:01:31 +0530

    search insert position

---
+++ arrays/LC_35_Search_Insert_Position.py
@@ -0,0 +1,26 @@
+# --------------------------------
+# Leetcode 35 - Search Insert Position
+# --------------------------------
+
+def searchInsert(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
+
+# --------------------------------
+# Examples 
+# -------------------------------
+
+print(searchInsert([1, 3, 5, 6], 5)) # Output: 2
+print()
+print(searchInsert([1, 3, 5, 6], 2)) # Output: 1
+print()
+print(searchInsert([1, 3, 5, 6], 7)) # Output: 4
